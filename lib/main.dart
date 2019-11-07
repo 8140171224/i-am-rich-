9a7022b1ad37d9b7d23213e1b3624665476e5fd8
Aakash Padhiyar <aakashpadhiyar06@gmail.com>
2019-11-07 09:57:42 +0530

    I am rich with center Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://img2.wikia.nocookie.net/__cb20120612022936/marvelcinematicuniverse/images/8/8e/324.jpg'),
+          ),
+        ),
       ),
     ),
   );
