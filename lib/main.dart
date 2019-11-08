339078df4f42be15512a9566502b27f770f52967
Aakash Padhiyar <aakashpadhiyar06@gmail.com>
2019-11-08 12:02:45 +0530

    text center

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I Am Rich'),
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
